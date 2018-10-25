57f87c26f492574abce1b511008f361fabd9624a
charlotte <charlotte.lusby@gmail.com>
2018-10-25 13:55:57 -0400

    potty mouth

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,7 +7,7 @@ public class MainApp {
 		System.out.println("Hello World!");
 		System.out.println("Goodbye, cruel world!");
 		System.out.println("It is too tough waking up knowing I will have to go back out there");
-System.out.println("As the calzone grease dripped down his chin he responded. ""Fuck I look like? Magellan?");
+System.out.println("As the calzone grease dripped down his chin he responded. Fiddle sticks I look like? Magellan?"); 
 	}
 
 }
