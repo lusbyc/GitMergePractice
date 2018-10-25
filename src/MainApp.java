97d6cd923454da9e14573c5f98ef5a19aa3d136a
charlotte <charlotte.lusby@gmail.com>
2018-10-25 13:46:25 -0400

    made it lovely

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,10 +2,11 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-System.out.println("Hello World!");
-System.out.println("Goodbye, cruel world!");
-System.out.println("It is too tough waking up knowing I will have to go back out there");
 
+		System.out.println("My name is Charlotte!");
+		System.out.println("Hello World!");
+		System.out.println("Goodbye, cruel world!");
+		System.out.println("It is too tough waking up knowing I will have to go back out there");
 
 	}
 
