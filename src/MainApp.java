b627c8a233ef8cb5888953ca03e6b3ada237cc98
chris <new826@gmail.com>
2018-10-25 13:35:52 -0400

    Suicide Note Added

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,6 +3,10 @@ public class MainApp {
 
 	public static void main(String[] args) {
 System.out.println("Hello World!");
+System.out.println("Goodbye, cruel world!");
+System.out.println("It is too tough waking up knowing I will have to go back out there");
+
+
 	}
 
 }
